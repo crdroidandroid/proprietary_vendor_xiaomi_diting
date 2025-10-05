12d803c94f79b752a23c19d5f5e7ffb4b3a32101
Mohammad Nt <mohammadnt96@gmail.com>
2025-10-05 23:13:54 +0330

    diting: inline with dt

--- diting-vendor.mk
+++ diting-vendor.mk
@@ -554,7 +554,6 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/diting/proprietary/vendor/firmware/T-handfree.txt:$(TARGET_COPY_OUT_VENDOR)/firmware/T-handfree.txt \
     vendor/xiaomi/diting/proprietary/vendor/firmware/T-handset.txt:$(TARGET_COPY_OUT_VENDOR)/firmware/T-handset.txt \
     vendor/xiaomi/diting/proprietary/vendor/firmware/T-music.txt:$(TARGET_COPY_OUT_VENDOR)/firmware/T-music.txt \
-    vendor/xiaomi/diting/proprietary/vendor/firmware/a730_sqe.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/a730_sqe.fw \
     vendor/xiaomi/diting/proprietary/vendor/firmware/a730_zap.b00:$(TARGET_COPY_OUT_VENDOR)/firmware/a730_zap.b00 \
     vendor/xiaomi/diting/proprietary/vendor/firmware/a730_zap.b01:$(TARGET_COPY_OUT_VENDOR)/firmware/a730_zap.b01 \
     vendor/xiaomi/diting/proprietary/vendor/firmware/a730_zap.b02:$(TARGET_COPY_OUT_VENDOR)/firmware/a730_zap.b02 \
@@ -616,7 +615,6 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/diting/proprietary/vendor/firmware/evautil64.b06:$(TARGET_COPY_OUT_VENDOR)/firmware/evautil64.b06 \
     vendor/xiaomi/diting/proprietary/vendor/firmware/evautil64.b07:$(TARGET_COPY_OUT_VENDOR)/firmware/evautil64.b07 \
     vendor/xiaomi/diting/proprietary/vendor/firmware/evautil64.mdt:$(TARGET_COPY_OUT_VENDOR)/firmware/evautil64.mdt \
-    vendor/xiaomi/diting/proprietary/vendor/firmware/gmu_gen70000.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/gmu_gen70000.bin \
     vendor/xiaomi/diting/proprietary/vendor/firmware/goodix_cfg_group_l12.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/goodix_cfg_group_l12.bin \
     vendor/xiaomi/diting/proprietary/vendor/firmware/goodix_cfg_group_l12_new.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/goodix_cfg_group_l12_new.bin \
     vendor/xiaomi/diting/proprietary/vendor/firmware/goodix_firmware_l12.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/goodix_firmware_l12.bin \
